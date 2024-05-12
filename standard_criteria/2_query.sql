5757f70d5820bed18bd1f974bac62656ba75bb18
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-12 20:05:34 +0200

    add second merged table

--- standard_criteria/2_query.sql
+++ standard_criteria/2_query.sql
@@ -2,3 +2,8 @@ CREATE TABLE SolarPower.Generation_Data AS
 SELECT * FROM `sql-sandbox-20240505.SolarPower.Plant_1_Generation_Data`
 UNION ALL
 SELECT * FROM `sql-sandbox-20240505.SolarPower.Plant_2_Generation_Data`
+
+CREATE TABLE SolarPower.Weather_Sensor_Data AS
+SELECT * FROM `sql-sandbox-20240505.SolarPower.Plant_1_Weather_Sensor_Data`
+UNION ALL
+SELECT * FROM `sql-sandbox-20240505.SolarPower.Plant_2_Weather_Sensor_Data`
