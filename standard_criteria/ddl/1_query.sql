9e6ec6fec63fe27ea80d638f98ddd73dd51799d4
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-12 20:38:24 +0200

    add data to tables via ddl

--- standard_criteria/ddl/1_query.sql
+++ standard_criteria/ddl/1_query.sql
@@ -2,3 +2,11 @@ CREATE TABLE students (
 	nmStudent varchar(255),
   courseID int
 )
+
+INSERT INTO students (nmStudent, courseID)
+VALUES
+	('Mark', 1),
+	('Jack', 2),
+	('Ivan', 3),
+	('Beth', 3),
+	('Sara', 6);
--- standard_criteria/ddl/2_query.sql
+++ standard_criteria/ddl/2_query.sql
@@ -1,4 +1,12 @@
 CREATE TABLE courses (
 	idCourse int,
 	nmCourse varchar(255)
-)
+);
+
+INSERT INTO courses (idCourse, nmCourse)
+VALUES
+	(1, 'Math'),
+	(2, 'English'),
+	(3, 'Physics'),
+	(4, 'Business'),
+	(5, 'History');
