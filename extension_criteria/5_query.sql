0ebeb113eebc80e947bdc4f755d11045c49d2d78
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-13 22:12:28 +0200

    query longest delivery times

---
+++ extension_criteria/5_query.sql
@@ -0,0 +1,8 @@
+SELECT category, product_id, AVG(DATETIME_DIFF(delivered_at, shipped_at, MINUTE)) AS avg_delivery_time
+FROM `bigquery-public-data.thelook_ecommerce.order_items` as order_items
+JOIN `bigquery-public-data.thelook_ecommerce.products` as products
+ON order_items.product_id=products.id
+GROUP BY product_id, category
+HAVING avg_delivery_time > 0
+ORDER BY avg_delivery_time DESC
+LIMIT 10
