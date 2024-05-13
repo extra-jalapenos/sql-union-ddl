b88a9e0039e7f4feb780cc0870b652fafcba7c12
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-13 21:51:25 +0200

    fix: wrong referenced join table

--- extension_criteria/1_query.sql
+++ extension_criteria/1_query.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM `bigquery-public-data.thelook_ecommerce.order_items` order_items
-JOIN `bigquery-public-data.thelook_ecommerce.order_items` as products
+JOIN `bigquery-public-data.thelook_ecommerce.products` as products
 ON order_items.product_id=products.id;
