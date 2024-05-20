b458b504352f2b96d92920a72717385eb3a78a50
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2024-05-20 10:06:43 +0200

    add answer text

--- extension_criteria/7_query.sql
+++ extension_criteria/7_query.sql
@@ -1,6 +1,10 @@
-SELECT Count(*) As Null_Count
+SELECT count(*), status
 FROM `bigquery-public-data.thelook_ecommerce.order_items` as order_items
 JOIN `bigquery-public-data.thelook_ecommerce.products` as products
 ON order_items.product_id=products.id
 WHERE delivered_at IS NULL
+GROUP BY status
 LIMIT 10;
+
+-- What do you think a null value means in such a column?
+--> it has not arrived yet or the order was cancelled
